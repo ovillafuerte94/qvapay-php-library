5aa0af1b36ad2a74842568e2b438eb39a877862c
Omar Villafuerte <ovillafuerte94@users.noreply.github.com>
2021-09-07 01:37:28 +0000

    Apply fixes from StyleCI

--- src/Client.php
+++ src/Client.php
@@ -7,42 +7,42 @@ use GuzzleHttp\Client as HttpClient;
 class Client
 {
     /**
-     * Application ID
+     * Application ID.
      *
      * @var string
      */
     protected $app_id;
 
     /**
-     * Application secret key
+     * Application secret key.
      *
      * @var string
      */
     protected $app_secret;
 
     /**
-     * Api version
+     * Api version.
      *
      * @var string
      */
     protected $version = 'v1';
 
     /**
-     * Api URL
+     * Api URL.
      *
      * @var string
      */
-    protected $api_url = "https://qvapay.com/api/";
+    protected $api_url = 'https://qvapay.com/api/';
 
     /**
-     * Http Client
+     * Http Client.
      *
      * @var GuzzleHttp\Client
      */
     protected $http_client;
 
     /**
-     * Construct
+     * Construct.
      *
      * @param array $config
      */
@@ -53,93 +53,98 @@ class Client
         $this->app_secret = $config['app_secret'];
 
         if (\array_key_exists('version', $config) && \is_numeric($config['version'])) {
-            $this->version = "v" . $config['version'];
+            $this->version = 'v'.$config['version'];
         }
 
         /* prepare http client */
         $this->http_client = new HttpClient([
-            'base_uri' => $this->api_url . $this->version . "/",
+            'base_uri' => $this->api_url.$this->version.'/',
         ]);
     }
 
     /**
-     * Get application information
+     * Get application information.
      *
      * @return mixed
      */
     public function info()
     {
-        return $this->request("info");
+        return $this->request('info');
     }
 
     /**
-     * Create payment invoice
+     * Create payment invoice.
      *
      * @param array $data
+     *
      * @return mixed
      */
     public function create_invoice(array $data)
     {
-        return $this->request("create_invoice", $data);
+        return $this->request('create_invoice', $data);
     }
 
     /**
-     * Gets transactions list, paginated by 50 items per request
+     * Gets transactions list, paginated by 50 items per request.
      *
      * @param int $page
+     *
      * @return mixed
      */
     public function transactions(int $page = 1)
     {
-        return $this->request("transactions", [
+        return $this->request('transactions', [
             'page' => $page,
         ]);
     }
 
     /**
-     * Get Transaction by UUID
+     * Get Transaction by UUID.
      *
      * @param string $uuid
+     *
      * @return mixed
      */
     public function get_transaction($uuid)
     {
-        return $this->request("transaction/" . $uuid);
+        return $this->request('transaction/'.$uuid);
     }
 
     /**
-     * Get your balance
+     * Get your balance.
      *
      * @return mixed
      */
     public function balance()
     {
-        return $this->request("balance");
+        return $this->request('balance');
     }
 
     /**
-     * Request to Qvapay Api
+     * Request to Qvapay Api.
+     *
      * @param string $path
-     * @param array $data
+     * @param array  $data
+     *
      * @return mixed
      */
     private function request($path, array $data = [], $method = 'GET')
     {
         /**
-         * Prepare options
+         * Prepare options.
          */
         $options = [
             'query' => ($method == 'GET') ? \array_merge($data, [
-                'app_id' => $this->app_id,
+                'app_id'     => $this->app_id,
                 'app_secret' => $this->app_secret,
             ]) : [
-                'app_id' => $this->app_id,
+                'app_id'     => $this->app_id,
                 'app_secret' => $this->app_secret,
             ],
         ];
 
         /**
-         * Method is POST
+         * Method is POST.
          */
         if ($method == 'POST') {
             $options['form_params'] = $data;
