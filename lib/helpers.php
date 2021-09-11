60acef99b64e031e50814ee8f448398027b0de22
Omar Villafuerte <ovillafuerte.94@gmail.com>
2021-09-11 16:45:15 -0400

    Internal changes (#23)

---
+++ lib/helpers.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Qvapay;
+
+/**
+ * Helpers Class.
+ *
+ * @category Abstract Class
+ *
+ * @author   Omar Villafuerte
+ *
+ * @link    https://ovillafuerte94.is-a.dev
+ */
+abstract class Helpers
+{
+    /**
+     * Check if a string is JSON.
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+    public static function isJson(string $string): bool
+    {
+        json_decode($string);
+
+        return json_last_error() === JSON_ERROR_NONE;
+    }
+}
